4d6e23422045453509ef9c1aff3928f24a47dbb4
Ian Wigley <ian.wigley@outlook.com>
2023-07-06 18:21:22 +0100

    Create main.ts

---
+++ Balls-VSCode/main.ts
@@ -0,0 +1,13 @@
+﻿require(["out/Balls.js"], function (BallDemo: any) {
+    let balls = new BallDemo();
+    balls.Initialize();
+    balls.Run();
+});
+
+require.config({
+    baseUrl: "out",
+    paths: {
+        "some": "Balls"
+    },
+    waitSeconds: 15,
+});
